7cc266dc556368a6ba274c4e9b91382f23f3a76d
Dylan BOLLARO <105074286+DylanBOLLARO@users.noreply.github.com>
2022-10-04 12:26:16 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,58 +1,77 @@
 const buildList = document.querySelector(".buildList");
 const mainView = document.querySelector(".mainView");
+let cardSelect = [];
 
 let dataJSON = [];
 let tempo = "";
+
+function showCard() {
+  cardSelect.forEach((element) => {
+    element.addEventListener("click", () => {
+      for (let i = 0; i < dataJSON.length; i++) {
+        if (element.id === dataJSON[i].Name) {
+          console.log(dataJSON[i].Name);
+          mainView.innerHTML = `<div class="card-name-bo">
+                                <h1 style="margin-bottom:20px">${element.id}</h1>
+                                </div>
+                              `;
+          mainView.innerHTML += `
+          <h2 style="margin-bottom:20px">Description:<p style="font-size:1rem;color:whitesmoke">${dataJSON[i].Description}</p></h2>
+        `;
+
+          mainView.innerHTML += `
+        <h2 style="margin-bottom:20px">Build order:</h2>
+      `;
+
+          mainView.innerHTML += `
+          <table>
+            <thead id="test">
+              <tr style="font-size:1.5rem">
+                <th scope="col">Population</th>
+                <th scope="col">Temps</th>
+                <th scope="col">Description</th>
+                <th scope="col">Indication</th>
+              </tr>
+            </thead>
+          </table>
+          `;
+          const inject = document.getElementById("test");
+
+          for (let j = 0; j < dataJSON[i].BO.length; j++) {
+            inject.innerHTML += `<tr  style="color:whitesmoke">
+                                  <th style="padding: 5px 20px" scope="col">${dataJSON[i].BO[j][0]}</th>
+                                  <th style="padding: 5px 20px" scope="col">${dataJSON[i].BO[j][1]}</th>
+                                  <th style="padding: 5px 20px;text-align:left" scope="col">${dataJSON[i].BO[j][2]}</th>
+                                  <th style="padding: 5px 20px"scope="col">${dataJSON[i].BO[j][3]}</th>
+                                </tr>`;
+          }
+
+          mainView.innerHTML += `
+        <h2 style="margin:20px 0">Suite:<p style="font-size:1rem;color:whitesmoke">${dataJSON[i].Suite}</p></h2>
+      `;
+        }
+      }
+    });
+  });
+}
 
 async function getBuildOrderJSON() {
   await fetch("buildOrder.json")
     .then((res) => res.json())
     .then((data) => {
       dataJSON = data;
-    });
-  // console.log(dataJSON);
-
-  for (let i = 0; i < dataJSON.length; i++) {
-    // console.log(dataJSON[i].Name);
-    buildList.innerHTML += `
-                            <div id="${dataJSON[i].Name}" class="card">
-                            <p>${dataJSON[i].Name}</p>
-                            </div>
-                          `;
-  }
-
-  const cardSelect = document.querySelectorAll(".card");
-  cardSelect.forEach((element) => {
-    element.addEventListener("click", () => {
-      // console.log("clic");
-      // console.log(element.id);
-      // console.log(dataJSON[0].Name);
       for (let i = 0; i < dataJSON.length; i++) {
-        if (element.id === dataJSON[i].Name) {
-          console.log(dataJSON[i].Name);
-          mainView.innerHTML = `
-                                <h1 style="margin-bottom:20px">${element.id}</h1>
+        buildList.innerHTML += `
+                                <div id="${dataJSON[i].Name}" class="card">
+                                <div><p>${dataJSON[i].Name}</p></div>                            
+                                </div>
                               `;
-          // console.log(dataJSON[i].BO.length);
-          mainView.innerHTML += `
-          <p style="margin-bottom:20px">${[
-            ["Population"],
-            ["Temps"],
-            ["Description"],
-          ].join(" ")}</p>
-        `;
-          for (let j = 0; j < dataJSON[i].BO.length; j++) {
-            mainView.innerHTML += `
-            <p>${dataJSON[i].BO[j].join(" ... ")}</p>
-          `;
-          }
-        }
-        // <div>${dataJSON[i].BO}</div>
       }
-
-      // console.log(dataJSON.includes(element.id));
+      cardSelect = document.querySelectorAll(".card");
+      console.log(cardSelect);
     });
-  });
+
+  showCard();
 }
 
 getBuildOrderJSON();
